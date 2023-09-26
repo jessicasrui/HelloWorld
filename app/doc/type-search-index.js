91a3d41ece02415fcf6852ba0a2432273eed6ec8
jessicasrui <jcastroruiz23@gmail.com>
2023-09-26 10:00:40 +0200

    Version sin aplicar .gitignore

---
+++ app/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.helloword","l":"ExampleInstrumentedTest"},{"p":"com.example.helloword","l":"ExampleUnitTest"},{"p":"com.example.helloword","l":"MainActivity"}];updateSearchResults();
---
+++ app/src/main/java/com/example/helloword/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.helloword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Aplicación que muestra el típico ejemplo de Hola Mundo
+ * @version 1.0
+ * <ol>
+ *     <li>Hemos visto como se crean los recursos en XML</li>
+ *     <li>Hemos instanciado en Java un ojbeto TextView</li>
+ *     <li>Se ha personalizado la imagen de la aplicación</li>
+ *     <li>Comprobando que funciona nuestro control de versiones</li>
+ * </ol>
+ */
+
+// el / con dos ** es para el javadoct, la documentacion de muestra aplicacion
+public class MainActivity extends AppCompatActivity {
+    private TextView tvMessageStart;
+    private TextView tvMessageEnd;
+
+    // TODO esto es un comentario para ver como se genera el comentario todo
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvMessageStart=findViewById(R.id.tvMessageStart);
+        tvMessageStart.setTextColor(getColor(R.color.white));   // esto equivale a android:text="@string/tvMessageStartText"
+        //tvMessageEnd.setText();
+    }
+
+}
+// plugin : atom material icon
+// ctrl+q : seleccionar un método o algo para ver ayuda
